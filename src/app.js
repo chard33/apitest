ae897fc5ba1d380e9d9dee343f106a90d72482d5
Chard33 <jchar33@hotmail.com>
2024-10-16 15:12:56 -0600

    Habilitando consultas 2.2

--- src/app.js
+++ src/app.js
@@ -3,12 +3,24 @@ const cors = require('cors');
 const conexion = require("./conexionBD")
 const app = express()
 
-app.use(cors()); 
+const corsOptions = {
+    origin: '*',  // Permitir todos los orígenes
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  // Métodos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'],  // Headers permitidos
+    credentials: true,  // Permitir envío de cookies o credenciales
+    maxAge: 3600  // Tiempo en segundos que se cachea la respuesta preflight
+};
+
+app.use(cors(corsOptions));
+
+app.options('*', cors(corsOptions));
 
 app.get("/cartas", (req, res) => {
 
-    res.header('Content-Type', 'application/json');
-    res.header('Warning', 'Se asignan los headers');
+    res.set({
+        'Content-Type': 'application/json',
+        'Warning': 'Este es un header adicional'
+      });
 
     conexion.query("SELECT * FROM `cartas`", (err, result) => {
         if (err) throw err;
