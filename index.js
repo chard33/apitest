94347981101ca8cad0fa6746655cdff300fd3997
Chard33 <jchar33@hotmail.com>
2024-10-16 10:49:34 -0600

    Agregando rutas

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ const puerto = process.env.PORT || 3000
 
 app.listen(puerto)
 
+app.get("/", (req, res) => {
+    res.send("Pagina inicio")
+})
+
+app.get("/cartas", (req, res) => {
+    res.send("Conexion a base de datos") 
+})
+
 console.log(`Iniciando en el puerto: ${puerto}`)
