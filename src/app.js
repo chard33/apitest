93e43d865f137e0db13c069af9b7b5e80384f0b8
Chard33 <jchar33@hotmail.com>
2024-10-16 13:36:01 -0600

    Api individual

--- src/app.js
+++ src/app.js
@@ -2,12 +2,6 @@ const express = require("express")
 const conexion = require("./conexionBD")
 const app = express()
 
-app.use(express.static('../public'))
-
-app.get("/", (req, res) => {
-    res.sendFile('index.html')
-})
-
 app.get("/cartas", (req, res) => {
     conexion.query("SELECT * FROM `cartas`", (err, result) => {
         if (err) throw err;
