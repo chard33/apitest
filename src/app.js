b471eea34ef292af19a7d22fd4b383a72ace8b20
Chard33 <jchar33@hotmail.com>
2024-10-16 14:40:30 -0600

    Habilitando consultas 1.8

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const conexion = require("./conexionBD")
 const app = express()
+const cors = require("cors")
 
-//app.use(cors());
+app.use(cors());
 
 app.get("/cartas", (req, res) => {
     conexion.query("SELECT * FROM `cartas`", (err, result) => {
