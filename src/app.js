fb61e6c52db290a76fbba370d55ac8d665ac915b
Chard33 <jchar33@hotmail.com>
2024-10-16 11:43:45 -0600

    Creacion consultas 1.5

--- src/app.js
+++ src/app.js
@@ -3,14 +3,17 @@ const conexion = require("./conexionBD")
 const app = express()
 
 app.get("/", (req, res) => {
-    
+
     res.send("Conexion a base de datos")
 })
 
 app.get("/cartas", (req, res) => {
     conexion.query("SELECT * FROM `cartas`", (err, result) => {
         if (err) throw err;
-        res.send(`${result}`)
+        var datos = result;
+
+        console.log(JSON.stringify(datos));
+        res.send(`${JSON.stringify(datos)}`)
     })
 })
 
